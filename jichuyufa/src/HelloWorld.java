fa99a176fbe0bf2cade012ae17d443f328abc95c
wangy <2297146709@qq.com>
2022-01-05 21:18:34 +0800

    javaSe第一次提交

---
+++ jichuyufa/src/HelloWorld.java
@@ -0,0 +1,18 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        //单行注释
+        //输出一个Hello,World
+        System.out.println("Hello, World");
+        /*
+        多行注释
+        可以注释一段文字
+         */
+
+        //JavaDoc: 文档注释
+        /**
+         * 文档注释
+         *
+         * @Author wyf
+         */
+    }
+}
